31631d4e5d46cead8ff23ef6af7d1f81a1f6dc99
Visinry (Jaden P) <78435427+visinry@users.noreply.github.com>
2021-11-15 19:27:11 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/BlockLetters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BlockLetters"
+  "BlockLetters.pdb"
+  "CMakeFiles/BlockLetters.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlockLetters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//I am a twin!
+
+int main ( ) {
+    std::cout << "JJJJJ  PPPP\n";
+    std::cout << "  J    P   P\n";
+    std::cout << "  J    P   P\n";
+    std::cout << "  J    PPPP\n";
+    std::cout << "J J    P\n";
+    std::cout << "J J    P\n";
+    std::cout << " JJ    P\n";
+}
